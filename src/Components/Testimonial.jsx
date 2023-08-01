ced4adbf4bbe7343dd62ade622ca4e84563445e6
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-01 20:09:51 +0530

    testimonial design

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,65 +1,38 @@
-import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper/modules';
+import React from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-export default () => {
+const Testimonial = () => {
   return (
-   <div className="testimonial">
-    <Swiper
-    effect={'coverflow'}
-    grabCursor={true}
-    centeredSlides={true}
-    loop={true}
-    slidesPerView={'auto'}
-    coverflowEffect={
-      {
-        rotateLeft:0,
-        stretchWidth:0,
-        depth:100,
-        modifiers: 2.5,
-
-      }
-
-    }
-    pagination={{el:".swiper-pagination",clickable:true}}
-    navigation={{
-      nextEl:".swiper-button-next",
-      prevEl:".swiper-button-prev",
-      clickable:true,
-    }}
-    className="Swiper-container"
-    modules={[EffectCoverflow,Pagination,Navigation]}
-    >
-      <SwiperSlide>
-        <img src="../assets/img/rev1.svg" alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="../assets/img/rev1.svg" alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="../assets/img/rev1.svg" alt="" />
-      </SwiperSlide>
-
-
-      <div className="slider-controler">
-        <div className="swiper-button-prev slider-arrow">
-          <ion-icon name="arrow-back-outline"></ion-icon>
-
+    <div className="testi-container">
+      <div className="testi-info">
+        <div className="testi-right">
+          <img src="../assets/img/rev2.svg" alt="" className="side-img"/>
         </div>
-        <div className="swiper-button-next slider-arrow">
-          <ion-icon name="arrow-forward-outline"></ion-icon>
-
+        <div className="testi-center">
+          <div className="center-img">
+            <img src="../assets/img/rev1.svg" alt="" />
+          </div>
+          <div className="testi-detail">
+            <h2>Teatimonial</h2>
+            <p>
+              Lorem Ipsum passage, and going through the cites of the word in
+              classical literature, discovered the undoubtable source.Lorem
+              Ipsum passage, and going through the cites of the word in
+              classical literature, Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, discovered the
+              undoubtable source.Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, Lorem Ipsum passage,
+              and going through the cites of the word in classical literature,
+              </p>
+            <h1>Phillip Franci</h1>
+            <h3>Selling Agents</h3>
+          </div>
         </div>
-        <div className="swiper-pagination"></div>
+        <div className="testi-left">
+          <img src="../assets/img/rev3.svg" alt="" className="side-img"/>
+        </div>
       </div>
-
-    </Swiper>
-   </div>
+    </div>
   );
 };
+
+export default Testimonial;
