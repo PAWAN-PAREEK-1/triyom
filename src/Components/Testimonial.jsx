ffc8b770504b588820a3b2fb789f6eb50bd50092
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-01 21:34:48 +0530

    testimonial update

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Testimonial = () => {
+  const testimonialData = [
+    {
+      testidec: "",
+      testiname: "huhhi",
+      testipost: "",
+      testiimg: "../assets/img/rev1.svg",
+      
+    },
+    {
+      testidec: "",
+      testiname: "hujhjhbhhi",
+      testipost: "",
+      testiimg: "../assets/img/rev2.svg",
+    },
+    {
+      testidec: "",
+      testiname: "hujhj5hbhhi",
+      testipost: "",
+      testiimg: "../assets/img/rev3.svg",
+    },
+    {
+      testidec: "",
+      testiname: "",
+      testipost: "",
+      testiimg: "../assets/img/rev2.svg",
+    },
+  ];
+
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonialData.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? testimonialData.length - 1 : prevIndex - 1
+    );
+  };
   return (
     <div className="testi-container">
       <img src="../assets/img/testibg.svg" alt="" id="testibg"/>
@@ -11,22 +51,15 @@ const Testimonial = () => {
         </div>
         <div className="testi-center">
           <div className="center-img">
-            <img src="../assets/img/rev1.svg" alt="" />
+            <img src={testimonialData[currentIndex].testiimg} alt="" />
           </div>
           <div className="testi-detail">
             <h2>Teatimonial</h2>
             <p>
-              Lorem Ipsum passage, and going through the cites of the word in
-              classical literature, discovered the undoubtable source.Lorem
-              Ipsum passage, and going through the cites of the word in
-              classical literature, Lorem Ipsum passage, and going through the
-              cites of the word in classical literature, discovered the
-              undoubtable source.Lorem Ipsum passage, and going through the
-              cites of the word in classical literature, Lorem Ipsum passage,
-              and going through the cites of the word in classical literature,
+            {testimonialData[currentIndex].testidec}
               </p>
-            <h1>Phillip Franci</h1>
-            <h3>Selling Agents</h3>
+            <h1>{testimonialData[currentIndex].testiname}</h1>
+            <h3>{testimonialData[currentIndex].testipost}</h3>
           </div>
         </div>
         <div className="testi-left">
@@ -34,8 +67,18 @@ const Testimonial = () => {
         </div>
       </div>
       <div className="navigation">
-        <img src="../assets/img/leftarrow.svg" alt="" />
-        <img src="../assets/img/rightarrow.svg" alt="" />
+      <img
+          src="../assets/img/leftarrow.svg"
+          alt=""
+          onClick={handlePrev}
+          className="arrow-icon"
+        />
+        <img
+          src="../assets/img/rightarrow.svg"
+          alt=""
+          onClick={handleNext}
+          className="arrow-icon"
+        />
       </div>
     </div>
   );
