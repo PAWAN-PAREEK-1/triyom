7ef19c48604480e26361d38ac1fb5d96483e28fd
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-04 20:26:40 +0530

    mobile nav update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,104 +10,77 @@ const Navbar = () => {
   };
 
   return (
-
     <div>
-    <nav>
-      <div className={`menuIcon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
-      </div>
+      <nav>
+        <div
+          className={`menuIcon ${menuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
 
-
-
-      <ul className="NavbarList ">
-        <li>
-          <NavLink to="/" >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/About" >
-            About us
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Project" >
-            Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Facilities" >
-            Facilities
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Reviews" >
-            Reviews
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Gallery" >
-            Gallery
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Faqs" >
-            FAQs
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Contact" >
-            Contact Us
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-    <div className='mobileNav'>
+        <ul className="NavbarList ">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/About">About us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Project">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Facilities">Facilities</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Reviews">Reviews</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Gallery">Gallery</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Faqs">FAQs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact">Contact Us</NavLink>
+          </li>
+        </ul>
+      </nav>
+      {menuOpen && (
+        <div className="mobileNav">
+          <div className="closebtn" onClick={() => setMenuOpen(false)}>
+            close
+          </div>
           <ul>
-             <li>
-          <NavLink to="/" >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/About" >
-            About us
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Project" >
-            Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Facilities" >
-            Facilities
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Reviews" >
-            Reviews
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Gallery" >
-            Gallery
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Faqs" >
-            FAQs
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Contact" >
-            Contact Us
-          </NavLink>
-        </li>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/About">About us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Project">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Facilities">Facilities</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Reviews">Reviews</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Gallery">Gallery</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Faqs">FAQs</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact">Contact Us</NavLink>
+            </li>
           </ul>
-     </div>
+        </div>
+      )}
     </div>
   );
 };
