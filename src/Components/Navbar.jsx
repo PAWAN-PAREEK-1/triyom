1bd667c442c7e9321edbff867e3327a2d81da1ae
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-04 20:07:51 +0530

    mobile nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
   };
 
   return (
+
+    <div>
     <nav>
       <div className={`menuIcon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <div className="bar1"></div>
@@ -17,49 +19,96 @@ const Navbar = () => {
         <div className="bar3"></div>
       </div>
 
-      <ul className={`NavbarList ${menuOpen ? 'open' : ''}`}>
+
+
+      <ul className="NavbarList ">
         <li>
-          <NavLink to="/" onClick={toggleMenu}>
+          <NavLink to="/" >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/About" onClick={toggleMenu}>
+          <NavLink to="/About" >
             About us
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Project" onClick={toggleMenu}>
+          <NavLink to="/Project" >
             Projects
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Facilities" onClick={toggleMenu}>
+          <NavLink to="/Facilities" >
             Facilities
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Reviews" onClick={toggleMenu}>
+          <NavLink to="/Reviews" >
             Reviews
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Gallery" onClick={toggleMenu}>
+          <NavLink to="/Gallery" >
             Gallery
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Faqs" onClick={toggleMenu}>
+          <NavLink to="/Faqs" >
             FAQs
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Contact" onClick={toggleMenu}>
+          <NavLink to="/Contact" >
             Contact Us
           </NavLink>
         </li>
       </ul>
     </nav>
+    <div className='mobileNav'>
+          <ul>
+             <li>
+          <NavLink to="/" >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/About" >
+            About us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Project" >
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Facilities" >
+            Facilities
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Reviews" >
+            Reviews
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Gallery" >
+            Gallery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Faqs" >
+            FAQs
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Contact" >
+            Contact Us
+          </NavLink>
+        </li>
+          </ul>
+     </div>
+    </div>
   );
 };
 
